06c5a8bfe36c9c952c7aaf0b434405dda9a4cd53
Echo Barros <diegobarros@mail.weber.edu>
2021-12-02 08:00:41 -0700

    day 1 done, no comments on second half

--- day1/day1.py
+++ day1/day1.py
@@ -6,9 +6,23 @@
 """ Counts how many times the depth increases in a file
 """
 
+def sliding_window():
+    """Counts how many times the three measurement sliding window increases
+    """
+    counter = 0
+    index = 0
+    with open('input', encoding='utf-8') as infile:
+        lines = infile.readlines()
+        previous_window = int(lines[0]) + int(lines[1]) + int(lines[2])
+        for i in range(1, len(lines) - 2):
+            current_window = int(lines[i]) + int(lines[i + 1]) + int(lines[i + 2])
+            if current_window > previous_window:
+                counter = counter + 1
+            previous_window = current_window
+        print(counter)
 
-def main():
-    """Main Driver
+def simple_increase():
+    """Counts how many times each depth increases in a file
     """
     counter = 0                                     # counts how many times the depth increases
     with open('input', encoding='utf-8') as infile: # opens the file
@@ -19,6 +33,11 @@ def main():
                 counter = counter + 1               # increment the counter
             previous_depth = current_depth          # set the previous_depth to the current_depth
     print(counter)                                  # after the loop, print the counter
+
+def main():
+    """Main Driver"""
+    simple_increase()
+    sliding_window()
 
 if __name__ == '__main__':
     main()
