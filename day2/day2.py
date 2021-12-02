01838d3dd4a8c92a22c5179bdc953e97414946da
Echo Barros <diegobarros@mail.weber.edu>
2021-12-02 08:52:19 -0700

    day 2 completed

--- day2/day2.py
+++ day2/day2.py
@@ -6,6 +6,21 @@
 """ Navigate a submarine
 """
 
+def aim_instructions():
+    aim = 0
+    depth = 0
+    horizontal = 0
+    with open('input', encoding='utf-8') as infile:
+        for line in infile:
+            if line[0] == 'f':
+                horizontal = horizontal + int(line[-2])
+                depth = depth + aim * int(line[-2])
+            elif line[0] == 'd':
+                aim = aim + int(line[-2])
+            elif line[0] == 'u':
+                aim = aim - int(line[-2])
+        print(depth * horizontal)
+
 def instructions():
     depth = 0
     horizontal = 0
@@ -23,7 +38,7 @@ def main():
     """Main Driver
     """
     instructions()
-
+    aim_instructions()
 if __name__ == '__main__':
     main()
 
